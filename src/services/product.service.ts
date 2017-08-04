3cf78d9c10defee63f37695641fad6cb075a2d66
PIERSON Tanguy <tpierson@lyreco.com>
2017-08-04 11:31:41 +0200

    debug filter suite 13

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -18,10 +18,26 @@ export class ProductService {
             selectedAttributes = selectedAttributes.concat(criterion.selectedAttribute);
         });
 
+        console.log('selected attributes : ');
+        console.log(selectedAttributes);
+
         let productsF = products.filter((product) => {
+            console.log('Product : ');
+            console.log(product);
             return selectedAttributes.every((attribute: any) => {
+                console.log('> selected attribute :');
+                console.log(attribute);
                 return product.attributes.some((productAttribute: any) => {
+                    console.log('  > product attribute');
+                    console.log(productAttribute);
                     return attribute.att_id === productAttribute.id && attribute.values.some((value: any) => {
+                        console.log('    > value');
+                        console.log(value);
+                        if (productAttribute.values.includes(value)) {
+                            console.log('      > Le produit contient cette valeur');
+                        } else {
+                            console.log('      > Le produit ne contient pas cette valeur');
+                        }
                         return productAttribute.values.includes(value)
                     })
                 })
